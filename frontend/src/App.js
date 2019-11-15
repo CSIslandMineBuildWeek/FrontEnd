5c0ae0feb7cbce3b5fcc1f52332dbef63632d08b
Melvin Merlos <melvinamerlos@gmail.com>
2019-11-14 22:43:38 -0500

    react app created

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Island Mine</p>
+        <a
+          className="App-links"
+          href="https://www.google.com"
+          target="_blank"
+        >
+          Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
