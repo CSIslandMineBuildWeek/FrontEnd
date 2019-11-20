ccb7b811bc8c8ccaf77370d2526c0e1d5a953c10
Melvin Merlos <melvinamerlos@gmail.com>
2019-11-19 23:30:59 -0500

    further styling and button config

--- island_miner/src/components/Footer.js
+++ island_miner/src/components/Footer.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="button">Explore</div>
+            <div className="explore button">Explore</div>
+            <div className="directions">
+                <p>Directional narration goes here</p>
+            </div>
+            <div className="footer-buttons">
+                <p className="button">N</p>
+                <p className="button">S</p>
+                <p className="button">W</p>
+                <p className="button">E</p>
+            </div>
         </div>
     )
 }
