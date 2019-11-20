20e544d1388c8a3433b9ec768abf6a26c6e7bdb5
Daniel Lim <danheelim@gmail.com>
2019-11-19 18:09:25 -0800

    started to reworks some changes

--- island_miner/src/components/Dots.js
+++ island_miner/src/components/Dots.js
@@ -8,10 +8,14 @@ import  newMap from '../GameMap.js'
 export default function Dots(props) {
     let xCoordin = props['room'].roomInfo.coordinates[0]
     let yCoordin = props['room'].roomInfo.coordinates[1]
+
+    const num = Math.sqrt(props.length)
+    const space = 500 / num;
     // console.log(props)
     let xValues = []
     let yValues = []
 
+    // these numbers are found using the highest_values function in splash page
     for(let i=50;i<74;i++){
         xValues.push(i)
     }
@@ -25,13 +29,14 @@ export default function Dots(props) {
 
     // xCoordin = xCoordin
     // yCoordin + index * 2
+
+    // console.log(props)
     return (
         <>
             <Circle
                 className="roomDot"
                 x={newX * 1000 / 73}
                 y={newY * 1000 / 74}
-  
                 offsetX={xCoordin + 400}
                 offsetY={yCoordin + 400}
                 radius={4}
